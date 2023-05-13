10b2993b04053aa50a8c2f664726f999882bc84e
Devin Altobello <dl.altobello@gmail.com>
2023-05-13 11:45:40 -0400

    Initial commit

--- src/order.js
+++ src/order.js
@@ -1,7 +1,41 @@
 
+// takeOrder
+function takeOrder(order, deliveryOrders){
+  if(deliveryOrders.length < 3 ){
+    deliveryOrders.push(order)
+  }
+  return deliveryOrders
+};
+
+// refundOrder
+function refundOrder(order, deliveryOrders){
+  // 1657 console.log(order)
+  // console.log(deliveryOrders)
+
+  for(var i = 0; i < deliveryOrders.length; i++){
+  if(deliveryOrders[i].orderNumber === order){
+    deliveryOrders.splice(i,1)
+    }
+  } 
+};
+
+// listItems
+function listItems(orderLog){
+  console.log(orderLog);
+  // for(var i = 0; i < deliveryOrders.length; i ++){
+  //   if(deliveryOrders[i].item === orderLog){
+  //   return deliveryOrders.item;
+  //   }
+  //  }
+};
+
+
+
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
+  takeOrder,
+  refundOrder,
+  listItems,
   // searchOrder
 }
+
