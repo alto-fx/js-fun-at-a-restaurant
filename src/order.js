cca9dc6a9b3575d5b7efa08c4142f411213e2604
Devin Altobello <dl.altobello@gmail.com>
2023-05-14 13:40:38 -0400

    Add listItems and searchOrder

--- src/order.js
+++ src/order.js
@@ -16,26 +16,64 @@ function refundOrder(order, deliveryOrders){
   if(deliveryOrders[i].orderNumber === order){
     deliveryOrders.splice(i,1)
     }
-  } 
-};
+  }
+}
 
 // listItems
-function listItems(orderLog){
-  console.log(orderLog);
-  // for(var i = 0; i < deliveryOrders.length; i ++){
-  //   if(deliveryOrders[i].item === orderLog){
-  //   return deliveryOrders.item;
-  //   }
-  //  }
+  function listItems(orderLog) {
+    newArray = [];
+    for (var i = 0; i < orderLog.length; i++) {
+      newArray.push(orderLog[i].item)
+      }
+      return newArray.join(', ')
+    }
+
+// searchOrder
+// 2 goals:
+// 1. does the order.item I am testing exactly match my test value? If yes, return true. If no, continue!
+// 2. does my test value exist among ANY known order.items in deliverOrders? If yes, return true, else return false.
+
+function searchOrder(deliveryOrders, testValue){
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item === testValue) {
+  return true
+    }
+  } 
+  return false 
 };
 
 
+
+
+
+// function searchOrder(deliveryOrders, testValue){
+//   var ret = false;
+//   // step 1
+//   for (var i = 0; i < deliveryOrders.length; i++) {
+//     if (deliveryOrders[i].item === testValue) {
+//      var ret = true;
+//       // step 2
+//     } else {
+//       for (var y = 0; y < deliveryOrders.length; y++) {
+//         if (deliveryOrders[y].item === testValue) {
+//           ret = true;
+//         }
+//     }
+//   }
+//   return ret;
+// }
+
+// function searchOrder(item, deliveryOrders){
+//   for (var i = 0; i < deliveryOrders.length; i++){
+// if(deliveryOrders[i].item === item){
+// } return 
+//     }
+//   };
 
 
 module.exports = {
   takeOrder,
   refundOrder,
   listItems,
-  // searchOrder
+  searchOrder
 }
-
