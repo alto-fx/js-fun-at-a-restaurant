54c8b03e1677cdae78ce14c930c00b6d794571f4
Devin Altobello <dl.altobello@gmail.com>
2023-05-18 21:14:13 -0400

    Unskip all completed tests

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,14 @@
 
+// it should have a name. it should be able to print "sexy pizza"
+function createRestaurant(name){
+var restaurantName = name
+  return restaurantName.name
+}
+
+
 
 module.exports = {
-  // createRestaurant, 
+  createRestaurant, 
   // addMenuItem,
   // removeMenuItem,
   // checkForFood
